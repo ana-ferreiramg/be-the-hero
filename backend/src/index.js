590fecf1bab44aaaf6773c7ea8d03517498cde79
Ana Ferreira <anapaulaferreira0309@gmail.com>
2020-07-30 15:19:46 -0300

    Add validation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express'); // importando o módulo express(pacote) para dentro da variável express
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes'); // importando as rotas(arquivo)
 
 const app = express(); // armazena a aplicação
@@ -7,6 +8,7 @@ const app = express(); // armazena a aplicação
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
 
